d588629aac1764dfe0cba74274c2ab138331e802
Uaychai Chotjaratwanich <chotjaratwanich@hotmail.com>
2025-12-14 16:57:33 +0700

    Added 202512141657

---
+++ 07_c503_CommonParameter/solution/command1.ps1
@@ -0,0 +1,50 @@
+Set-Location -Path "$PSScriptRoot/files"
+#----------------------------------------------
+
+#Get-ChildItem -OutVariable filelist1
+#$filelist2 = Get-ChildItem
+#$filelist1
+#$filelist2
+
+#----------------------------------------------
+
+#New-Item -Name "newfile1.txt" -ItemType "File" -Value "This is a new file." 
+#New-Item -Name "newfile2.txt" -ItemType "File" -Value "This is a new file." -Verbose
+
+#----------------------------------------------
+
+# Write-Host "`$DebugPreference=$DebugPreference"
+# Write-Host "`$InformationPreference=$InformationPreference"
+# Write-Host "`$WarningPreference=$WarningPreference"
+# Write-Host "`$ErrorActionPreference=$ErrorActionPreference"
+
+# $DebugPreference = "SilentlyContinue"
+# Remove-Item -Path "newfile1.txt" `
+#     -InformationAction Continue -WarningAction Continue -ErrorAction Continue -ProgressAction Continue -Debug `
+#     -InformationVariable infoVar -WarningVariable warnVar -ErrorVariable errVar; 
+# Write-Host "`$inforVar=$infoVar`n`$warnVar=$warnVar`n`$errVar=$errVar`n`$Error=$Error";
+
+#----------------------------------------------
+
+# Get-ChildItem -PipelineVariable fileitem | 
+#     Get-Content -PipelineVariable lineitem | 
+#     ForEach-Object {Write-Host "File: $($fileitem.Name) Line: $lineitem"}
+
+#----------------------------------------------
+
+# Remove-Item -Path .\file1.txt -WhatIf
+# Remove-Item -Path .\file2.txt -Confirm
+
+#----------------------------------------------
+
+# 1..4 | ForEach-Object {
+#         Write-Host "$($_): First"; $_
+#       }  | ForEach-Object {
+#                         Write-Host "$($_): Second" }
+
+# 1..4 | ForEach-Object {
+#         Write-Host "$($_): First"; $_
+#       } -OutBuffer 0 | ForEach-Object {
+#                         Write-Host "$($_): Second" }
+
+#----------------------------------------------
---
+++ 08_cb84_ControlStatement/solution/command5.ps1
@@ -0,0 +1,27 @@
+# try {
+#     $a = 5;
+#     $b = 0;
+#     $c = $a / $b;
+#     Write-Host "The result is $c"
+# }
+# catch {
+#     Write-Host "Something went wrong";
+#     Write-Host "$Error `n"
+#     Write-Host "$($_.Exception.Message)"
+# }
+# finally{
+#     $Error.Clear();
+#     Write-Host "Execution of the script is complete."
+# }
+
+#----------------------------------------------------------
+
+# try {
+#     Set-Location -Path "C:\Projects\MyApp" -ErrorAction Stop
+# }
+# catch {
+#     Write-Host "The specified path does not exist."
+# }
+# finally{
+#     Write-Host "Execution of the script is complete."
+# }
