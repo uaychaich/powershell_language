a295cbc24bebc815dcbb6c95884f97585b13f526
Uaychai Chotjaratwanich <chotjaratwanich@hotmail.com>
2025-12-30 10:45:07 +0700

    Added 202512301050

--- 11_f642_Remote/solution/command.ps1
+++ 11_f642_Remote/solution/command.ps1
@@ -27,3 +27,39 @@
 
 #-------------------------------------------------
 
+# Set-Location $PSScriptRoot
+# Get-PSSession | Remove-PSSession -Confirm
+# $uaysession = New-PSSession -Name UaySession -ComputerName localhost
+# Get-PSSession | Format-Table -AutoSize
+# Invoke-Command -Session $uaysession -ScriptBlock {
+#     Start-Sleep -Seconds 15
+#     "Background job complete"
+#     Get-Process -Name pwsh
+# } -AsJob -JobName "UayRemoteJob1" 
+# Get-Job -Name "UayRemoteJob1"
+# Receive-Job -Name "UayRemoteJob1" -Wait -AutoRemoveJob
+
+#-------------------------------------------------
+
+# Set-Location $PSScriptRoot
+# Get-PSSession | Remove-PSSession -Confirm 
+# $uaysession = New-PSSession -Name UaySession -ComputerName localhost
+# Get-PSSession | Format-Table -AutoSize
+# Invoke-Command -Session $uaysession -ScriptBlock {
+#     Start-Sleep -Seconds 15
+#     "Background job complete"
+#     Get-Process -Name pwsh
+# } -AsJob -JobName "UayRemoteJob1" 
+# Disconnect-PSSession -Session $uaysession
+# Write-Host "Session disconnected. Checking job status..."
+# Get-Job -Name "UayRemoteJob1"
+# Get-PSSession
+# Receive-PSSession -Session $uaysession -OutTarget Job -JobName "UayRemoteJob1"
+# Write-Host "Reconnected to session. Checking job status..."
+# Get-Job -Name "UayRemoteJob1"
+# Get-PSSession
+# Receive-Job -Name "UayRemoteJob1" -Wait -AutoRemoveJob
+
+#-------------------------------------------------
+
+
